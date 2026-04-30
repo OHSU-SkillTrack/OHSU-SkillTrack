c169b0d4b3649d982b4ea7dc03dca13a3b975a04
Aaron-40 <140855821+Aaron-40@users.noreply.github.com>
2026-04-30 01:28:51 -0700

    finished endpoint to add the resource card

--- Backend/SkillTrackBackend/AddDrugCardToUser/app.py
+++ Backend/SkillTrackBackend/AddDrugCardToUser/app.py
@@ -45,7 +45,31 @@ def add_drug_card_to_user(event, context):
 
 
         #now we extract each particular input from the 
-            
+        CardID = str(CardID) #reconvert to a string as that is the format we will actually need it in
+
+        genericName = input_body.get('genericName' , '')
+        tradeName = input_body.get('tradeName' , '')        
+        pronunciation = input_body.get('pronunciation' , '')
+        therapeuticClass = input_body.get('therapeuticClass' , '')
+        dose = input_body.get('dose' , '')
+        routes = input_body.get('routes' , '')
+        timeFrequency = input_body.get('timeFrequency' , '')
+        normalDosageRange = input_body.get('normalDosageRange' , '')
+        onset = input_body.get('onset' , '')        
+        peak = input_body.get('peak' , '')
+        duration = input_body.get('duration' , '')
+        ivCompatibility = input_body.get('ivCompatibility' , '')
+        whyPatientGettingMed =input_body.get('whyPatientGettingMed' , '')
+        nursingImplications =input_body.get('nursingImplications' , '')
+        mechanismOfAction = input_body.get('mechanismOfAction' , '')
+        commonSideEffects = input_body.get('commonSideEffects' , '')
+        interactions = input_body.get('interactions' , '')
+        labValueAlterations = input_body.get('labValueAlterations' , '')
+        patientTeaching =input_body.get('patientTeaching' , '')
+        nursingAssessment = input_body.get('nursingAssessment' , '')
+        holdAssessment = input_body.get('holdAssessment' , '')
+        evaluation =input_body.get('evaluation' , '')         
+
 
     except:
         statusCode = 400
@@ -75,48 +99,61 @@ def add_drug_card_to_user(event, context):
 
     #here we go ahead and cosnstruct the drug card based on what was provided
     try:
+
+        #first we create the new drug card
         newDrugCard = {
-            CardID : {
-                'genericName': '',
-                'tradeName': '',
-                'pronunciation': '',
-                'therapeuticClass': '',
-                'dose': '',
-                'routes': '',
-                'timeFrequency': '',
-                'normalDosageRange': '',
-                'onset': '',
-                'peak': '',
-                'duration': '',
-                'ivCompatibility': '',
-                'whyPatientGettingMed': '',
-                'nursingImplications': '',
-                'mechanismOfAction': '',
-                'commonSideEffects': '',
-                'interactions': '',
-                'labValueAlterations': '',
-                'patientTeaching': '',
-                'nursingAssessment': '',
-                'holdAssessment': '',
-                'evaluation': '',
-            }
+            'genericName': genericName,
+            'tradeName': tradeName,
+            'pronunciation': pronunciation,
+            'therapeuticClass': therapeuticClass,
+            'dose': dose,
+            'routes': routes,
+            'timeFrequency': timeFrequency,
+            'normalDosageRange': normalDosageRange,
+            'onset': onset,
+            'peak': peak,
+            'duration': duration,
+            'ivCompatibility': ivCompatibility,
+            'whyPatientGettingMed': whyPatientGettingMed,
+            'nursingImplications': nursingImplications,
+            'mechanismOfAction': mechanismOfAction,
+            'commonSideEffects': commonSideEffects,
+            'interactions': interactions,
+            'labValueAlterations': labValueAlterations,
+            'patientTeaching': patientTeaching,
+            'nursingAssessment': nursingAssessment,
+            'holdAssessment': holdAssessment,
+            'evaluation': evaluation,
         }
+
+
+
+        #now we add it to the table 
+        table.update_item(
+            Key ={
+                "ID": f"USER_RESOURCES_FOR#{calling_user_email}"
+            },
+            UpdateExpression= "SET #DrugCards.#CURRENTCARD = :card",
+            ExpressionAttributeNames={
+                "#DrugCards":    'DrugCards',
+                '#CURRENTCARD':   str(CardID)
+            },
+            ExpressionAttributeValues={
+                ":card": newDrugCard
+            }
+        )
 
     except Exception as e:
         statusCode = 500
         output_body = "failed create drug card. Finished with this error: " + str(e)
-
-    return{
-        "statusCode": statusCode,
-        "headers": GlobalHeaders,
-        "body": json.dumps(output_body)
-    }
+        return{
+            "statusCode": statusCode,
+            "headers": GlobalHeaders,
+            "body": json.dumps(output_body)
+        }
 
 
     
-
-
-
 
 
 
